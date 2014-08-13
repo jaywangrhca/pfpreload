fc88600a27e32bb22512a0b469855b25087e35fb
Jay Wang <jaywangrhca@yahoo.com>
2014-08-14 05:46:28 +0800

    add thread limitation

--- option.rb
+++ option.rb
@@ -1,19 +1,28 @@
+require 'thread'
 require 'optparse'
 options = {}
 opt_parser = OptionParser.new do |opts|
     opts.banner = "Usage: opt.rb -t 3 -d 3 -i sha-acu-dlc:1888 -u test -p test [options]"
 
     opts.on('-t N', '--thread NUMBER', Integer, 'How many threads runing together(1-20)') do |value|
-        #TODO: if condition for value in 1-20
+        if (1..20).include?(value)
             options[:thread] = value
-            #raise 'Thread number out of scope(1-20)'
+        else
+            raise 'Thread number is out of scope(1-20)'
+        end
     end
 
     opts.on('-d N', '--depth NUMBER', Integer, 'How deep would the dir look for(1-3)') do |value|
-        options[:depth] = value
+        if (1..3).include?(value)
+            options[:depth] = value
+        else
+            raise 'Depth is out of scope(1-3)'
+        end
     end
 
     opts.on('-i INSTANCE', '--instance INSTANCE', 'Instance information') do |value|
+        #TODO:
+        # option requirment
         options[:instance] = value
     end
 
@@ -26,15 +35,14 @@ opt_parser = OptionParser.new do |opts|
     end
 end.parse!
 
-
 p options
-p ARGV
+
 ROOT_DIR = './root'
 dirs = []
 case options[:depth]
 when 1
     dirs = %x{ ls -1d #{ROOT_DIR}/*/ }.split
-    #dirs = p4 dir
+    #dirs = pfdir(path)
 when 2
     dirs = %x{ ls -1d #{ROOT_DIR}/*/*/ }.split
 when 3
@@ -43,5 +51,48 @@ else
     puts "nothing"
 end
 
-p dirs
-#TODO: thread limitation test
+if options[:thread] > dirs.count
+    num_of_thread = dirs.count
+else
+    num_of_thread = options[:thread]
+end
+
+mutex = Mutex.new
+resource = ConditionVariable.new
+count = 0
+threads = []
+dirs.each do |dir|
+    threads << Thread.new {
+        mutex.synchronize {
+            while count > num_of_thread
+                resource.wait(mutex)
+            end
+            count +=1
+            print "first count: "
+            puts count
+        }
+        preload(dir)
+        mutex.synchronize {
+            count -=1
+            print "second cont: "
+            puts count
+            resource.signal
+        }
+    }
+end
+threads.each do |td|
+    # TODO
+    td.join
+end
+def preload(dir, user=options[:user], passwd=options[:passwd], instance=options[:instance])
+    # TODO
+    # p4 login
+    # check old process
+    # write log to file
+    # mail
+    puts "variable: #{dir}, #{user}, #{passwd}, #{instance}"
+end
+
+def pfdir(path, user=options[:user], passwd=options[:passwd], instance=options[:instance])
+    # TODO
+end
