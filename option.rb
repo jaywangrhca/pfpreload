710659c066d9e975be1bc4144dd1f7cd1bb86897
Jay Wang <jaywangrhca@gmail.com>
2014-08-29 00:56:35 +0000

    Add log operation

--- option.rb
+++ option.rb
@@ -18,7 +18,7 @@ def preload(dir, user, passwd, instance, client)
     puts "preloading dir: #{dir}"
     preload_log_file = "#{$log_dir}/this_time_synced#{dir.gsub(/\/| /,'_')}"
     if ! system("/usr/bin/pgrep -f '#{instance} -Zproxyload sync #{dir}'")
-        $logger.info("Start to sync #{dir}!")
+        $logger.info("Start to sync #{dir} !")
         %x(
     export P4USER=#{user}
     export P4PASSWD=#{passwd}
@@ -27,10 +27,9 @@ def preload(dir, user, passwd, instance, client)
     export PATH=/opt/perforce/git-fusion/bin:/opt/perforce/git-fusion/libexec:/opt/perforce/usr/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/X11R6/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/perforce/bin:/home/perforce/bin:/opt/perforce/bin/p4
     /opt/perforce/bin/p4 -p #{instance} -Zproxyload sync #{dir}/... &>#{preload_log_file}
         )
-        $logger.info("Sync #{dir} is done!")
+        $logger.info("Sync #{dir} is done !")
     else
-        $logger.warn("Old process is syncing #{dir}!")
-        %x(touch "#{$log_dir}/last_time_still_running#{dir.gsub(/\/| /,'_')}")
+        $logger.warn("Old process is syncing #{dir} !")
     end
 end
 
@@ -65,9 +64,9 @@ opt_parser = OptionParser.new do |opts|
         options[:depth] = value
     end
 
-    #opts.on('-l LOG', '--log LOG', 'Log file to save logs') do |value|
-    #    options[:log] = value
-    #end
+    opts.on('-l LOG', '--log LOG', 'Log file to save logs') do |value|
+        options[:log] = value
+    end
 
     opts.on('-r ROOTPATH', '--root ROOTPATH', 'What is the preload root path') do |value|
         options[:root] = value
@@ -101,7 +100,7 @@ begin
         raise OptionParser::InvalidArgument, 'Thread number is out of scope(1-20)'
     end
     if not (0..2).include?(options[:depth])
-        raise OptionParser::InvalidArgument, 'Depth number is out of scope(1-2)'
+        raise OptionParser::InvalidArgument, 'Depth number is out of scope(0-2)'
     end
 rescue OptionParser::InvalidOption, OptionParser::MissingArgument, OptionParser::InvalidArgument
     puts $!.to_s
@@ -111,18 +110,19 @@ end
 
 epoch = Time.now.to_i
 
-log_name = "/tmp/#{epoch}_#{options[:instance]}.log}"
+host_name = `hostname`
+log_name = options[:log] || "/tmp/#{epoch}_#{options[:instance]}.log"
 puts "Log file is #{log_name}"
 log_file = File.open(log_name, 'w')
 $logger = Logger.new(log_file)
 $logger.formatter = proc do |severity, datetime, progname, msg|
-      "#{datetime} #{severity}: #{msg}\n"
+    "#{datetime} #{severity}: #{msg}\n"
 end
 $logger.info("Program start:")
 $logger.info("for instance: #{options[:instance]}, root path: #{options[:root]}, depth #{options[:depth]}, threads:  #{options[:thread]}")
 
 connection = login_test(options[:user], options[:passwd], options[:instance], options[:client])
-if   connection =~ /^$/
+if connection =~ /^$/
     $logger.error("Connection test failed! Check username, password and client!")
     raise "Connection test failed, please check instance, username, passwrod!"
 end
@@ -140,7 +140,7 @@ end
 
 puts "num of thread is #{num_of_thread}"
 
-$logger.info("Actually thread is #{num_of_thread}!")
+$logger.info("Real thread is #{num_of_thread} !")
 
 $log_dir = "/tmp/#{options[:instance]}/#{epoch}"
 $logger.info("Logs for preload are in #{$log_dir}")
@@ -163,7 +163,6 @@ dirs.each do |dir|
             count +=1
         }
         preload(dir, options[:user], options[:passwd], options[:instance], options[:client])
-        #sleep rand(5)
         mutex.synchronize {
             count -=1
             resource.signal
@@ -179,12 +178,12 @@ $logger.close
 #log_file.close
 log_contents = File.read(log_name)
 message = <<MESSAGE_END
-From: Private Person <zu-jie.wang@ubisoft.com>
-To: A Test User <zu-jie.wang@ubisoft.com>
-Subject: SMTP e-mail test
+From: #{host_name } <root@ubisoft.com>
+To: APAC GNS PRODUCTION <zu-jie.wang@ubisoft.com>
+Subject: #{host_name} instance #{options[:instance]} preload
 #{log_contents}
 MESSAGE_END
 
 Net::SMTP.start('localhost') do |smtp|
-    smtp.send_message message, 'zu-jie.wang@ubisoft.com', 'zu-jie.wang@ubisoft.com'
+    smtp.send_message message, 'root@ubisoft.com', 'zu-jie.wang@ubisoft.com'
 end
